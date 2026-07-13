17af2406be707234a012399ffc2214e3019bda50
ma7zjar-lab <ma7zjar@gmail.com>
2026-07-14 00:12:55 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,53 @@
+# ==============================
+# Discord Bot Configuration
+# ==============================
+
+# Put your bot token here at the end of the project
+# DO NOT share this with anyone
+TOKEN = ""
+
+# ==============================
+# Command Access
+# ==============================
+
+# Only these Discord user IDs can use bot commands
+# Replace these example IDs with your own Discord ID
+
+OWNER_IDS = {
+    972434666948808724,
+}
+
+
+# ==============================
+# Database
+# ==============================
+
+DATABASE = "data/bot.db"
+
+
+# ==============================
+# Embed Settings
+# ==============================
+
+SUCCESS_COLOR = 0x57F287
+ERROR_COLOR = 0xED4245
+WARNING_COLOR = 0xFEE75C
+INFO_COLOR = 0x5865F2
+
+FOOTER_TEXT = "Moderation Bot"
+
+
+# ==============================
+# Logging
+# ==============================
+
+# Put your log channel ID here later
+LOG_CHANNEL_ID = https://discord.com/channels/1398950429320155166/1398996215236001815
+
+
+# ==============================
+# Prefix
+# ==============================
+
+# Prefix commands (!ban, !kick, etc.)
+PREFIX = "!"
